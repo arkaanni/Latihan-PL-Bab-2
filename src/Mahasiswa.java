d8a9db28dd8e2add499e1cb9199ee7f1a7e6220c
MINE <arkaan.smait7@gmail.com>
2016-03-09 21:00:27 +0700

    update sourcecode, update screenshot

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -1,121 +1,54 @@
 public class Mahasiswa{
-	private int jumlah_mahasiswa;
-	private String[] nama_mahasiswa = new String[1000];
-	private long[] nim = new long[1000];
-	private int[] jumlahPutaran = new int[1000];
-	private int[] jumlahWaktu = new int[1000];
-<<<<<<< HEAD
-    private final int[] jarak = new int[1000];
-    private final int[] kecepatan = new int[1000];
-	private int cekWaktu;
-	private int cekKecepatan;
-	
-	public Mahasiswa(String[] nama, long[] nim, int[] putaran, int[] waktu){
+    private int jumlah_mahasiswa;       
+    private String[] nama_mahasiswa = new String[1000];
+    private long[] nim = new long[1000];
+    private double[] jumlahPutaran = new double[1000];
+    private double[] jumlahWaktu = new double[1000];
+    private double[] jarak = new double[1000];
+	/*private double[] kecepatan = new double[1000];*/
+    private double[] waktuTercepat = new double[1000];
+    private double cekWaktu;
+    
+    public Mahasiswa(String[] nama, long[] nim, double[] putaran, double[] waktu){
         nama_mahasiswa = nama;
         this.nim = nim;
         jumlahPutaran = putaran;
-		jumlahWaktu = waktu;
-	}
-	public void Jarak(){
+        jumlahWaktu = waktu;
+    }
+    public void Jarak(){
+        for(int i=0; i<nim.length; i++){
+            jarak[i] = Lapangan.getKeliling()*jumlahPutaran[i];
+        }
+    }
+	/*public void kecepatan(){
 		for(int i=0; i<nim.length; i++){
-			jarak[i] = Lapangan.getKeliling()*jumlahPutaran[i];
-		}
-	}
-	public void Kecepatan(){
-		for(int i=0; i<nim.length; i++){
-                kecepatan[i] = jarak[i]/(jumlahWaktu[i]*60);
-			}
-	}
-	public String waktuTercepat(){
-		cekWaktu = jumlahWaktu[0];
+            kecepatan[i] = jarak[i]/jumlahWaktu[i];
+        }
+	}*/
+    public void waktuTercepat(){
+        for(int i=0; i<nim.length; i++){
+            waktuTercepat[i] = jumlahWaktu[i]/jumlahPutaran[i];
+        }
+    }
+    public String pemenang(){
+        cekWaktu = waktuTercepat[0];
 		String cek = "";
         for(int i=0; i<nim.length; i++){
-			if(jumlahWaktu[i]<cekWaktu){
-				cekWaktu = jumlahWaktu[i];
-                if(jumlahWaktu[i]==cekWaktu){
-					cek = nama_mahasiswa[i]; 
-				}
-            }
-		}
-		return cek;
-	}
-	public String pemenang(){
-		cekKecepatan = kecepatan[0];
-        String cek = "";
-        for(int i=0; i<nim.length; i++){
-			if(kecepatan[i]>cekKecepatan){
-                cekKecepatan = kecepatan[i];
-                if(kecepatan[i]==cekKecepatan){
-					cek = nama_mahasiswa[i];
+            if(waktuTercepat[i]<cekWaktu){
+                cekWaktu = waktuTercepat[i];
+                if(waktuTercepat[i]==cekWaktu){
+                    cek = nama_mahasiswa[i]; 
                 }
             }
         }
         return cek;
     }
-	public void displayData(){
+    public void displayData(){
         for(int i=0; i<nim.length; i++){
-			System.out.println("nama             : "+nama_mahasiswa[i]);
+            System.out.println("nama             : "+nama_mahasiswa[i]);
             System.out.println("jarak            : "+jarak[i]+" m");
         }
-        System.out.println("waktu tercepat   : "+waktuTercepat()+" "+cekWaktu+" menit");
-		System.out.println("pemenang         : "+pemenang()+" "+cekKecepatan+" m/s");
-	}
+        System.out.println("pemenang         : "+pemenang()+" "+cekWaktu+" menit/putaran");
+    }
 	
 }
-=======
-        private final int[] jarak = new int[1000];
-        private final int[] kecepatan = new int[1000];
-	
-	public Mahasiswa(String[] nama, long[] nim, int[] putaran, int[] waktu){
-            nama_mahasiswa = nama;
-            this.nim = nim;
-            jumlahPutaran = putaran;
-            jumlahWaktu = waktu;
-	}
-	public void Jarak(){
-            for(int i=0; i<nim.length; i++){
-                jarak[i] = Lapangan.getKeliling()*jumlahPutaran[i];
-            }
-	}
-        public void Kecepatan(){
-            for(int i=0; i<nim.length; i++){
-                kecepatan[i] = jarak[i]/jumlahWaktu[i];
-            }
-        }
-        public String waktuTercepat(){
-            int cek = jumlahWaktu[0];
-            String cek2 = "";
-            for(int i=0; i<nim.length; i++){
-                if(jumlahWaktu[i]<cek){
-                    cek = jumlahWaktu[i];
-                    if(jumlahWaktu[i]==cek){
-                       cek2 = nama_mahasiswa[i]; 
-                    }
-                }
-            }
-            return cek2;
-        }
-        public String pemenang(){
-            int cek = kecepatan[0];
-            String cek2 = "";
-            for(int i=0; i<nim.length; i++){
-                if(kecepatan[i]>cek){
-                    cek = kecepatan[i];
-                    if(kecepatan[i]==cek){
-                        cek2 = nama_mahasiswa[1];
-                    }
-                }
-            }
-            return cek2;
-        }
-	public void displayData(){
-            for(int i=0; i<nim.length; i++){
-                System.out.println("nama            : "+nama_mahasiswa[i]);
-                System.out.println("jarak           : "+jarak[i]);
-            }
-            System.out.println("waktu tercepat  : "+waktuTercepat());
-            System.out.println("pemenang        : "+pemenang());
-	}
-		
-}
->>>>>>> 8ad04bca624a7c9d5d0246be0e42f95657db7afc
--- src/MainMahasiswa.java
+++ src/MainMahasiswa.java
@@ -3,29 +3,17 @@ import java.util.Scanner;
 public class MainMahasiswa{
     public static void main(String[] args){
     	Scanner in = new Scanner(System.in);
-<<<<<<< HEAD
 	System.out.print("jumlah mahasiswa     : ");
 	int jm = in.nextInt();
 	System.out.print("panjang lapangan (m) : ");
 	Lapangan.setPanjang(in.nextInt());
 	System.out.print("lebar lapangan (m)   : ");
-=======
-	System.out.print("jumlah mahasiswa : ");
-	int jm = in.nextInt();
-	System.out.print("panjang lapangan : ");
-	Lapangan.setPanjang(in.nextInt());
-	System.out.print("lebar lapangan   : ");
->>>>>>> 8ad04bca624a7c9d5d0246be0e42f95657db7afc
 	Lapangan.setLebar(in.nextInt());
 	in.nextLine();
         String[] nama = new String[jm];
         long[] nim = new long[jm];
-        int[] jumlahputaran = new int[jm], jumlahwaktu = new int[jm];
-<<<<<<< HEAD
+        double[] jumlahputaran = new double[jm], jumlahwaktu = new double[jm];
 	System.out.println("\ninput ");
-=======
-	System.out.println("input ");
->>>>>>> 8ad04bca624a7c9d5d0246be0e42f95657db7afc
 	for(int i=0; i<jm; i++){
             System.out.print("nim              : ");
             nim[i] = in.nextLong();
@@ -33,26 +21,16 @@ public class MainMahasiswa{
             System.out.print("nama mahasiswa   : ");
             nama[i] = in.nextLine();
             System.out.print("jumlah putaran   : ");
-            jumlahputaran[i] = in.nextInt();
-<<<<<<< HEAD
+            jumlahputaran[i] = in.nextDouble();
             System.out.print("jumlah waktu (m) : ");
-=======
-            System.out.print("jumlah waktu     : ");
->>>>>>> 8ad04bca624a7c9d5d0246be0e42f95657db7afc
-            jumlahwaktu[i] = in.nextInt();
+            jumlahwaktu[i] = in.nextDouble();
             in.nextLine();	
 	}
 	Mahasiswa mhs = new Mahasiswa(nama, nim, jumlahputaran, jumlahwaktu);
         mhs.Jarak();
-        mhs.Kecepatan();
-<<<<<<< HEAD
+        /*mhs.kecepatan();*/
+        mhs.waktuTercepat();
         System.out.println("\noutput ");
         mhs.displayData();
     }
 }
-=======
-        System.out.println("output ");
-        mhs.displayData();
-    }
-}
->>>>>>> 8ad04bca624a7c9d5d0246be0e42f95657db7afc
